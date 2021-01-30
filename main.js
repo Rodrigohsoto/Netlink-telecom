08c0f06cf1e73eefe5e69d0503a37afcdf613514
rodrigohsoto <rodrigohsoto23@gmail.com>
2021-01-29 20:43:37 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector(".menuboton").addEventListener("click", () => {
+    document.querySelector(".navmenu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.banner');
+  ScrollReveal().reveal('.noticias', { delay: 500});
+  ScrollReveal().reveal('.banner2', { delay: 500});
+  ScrollReveal().reveal('.noticias', { delay: 500});
+  ScrollReveal().reveal('.banner3', { delay: 500});
+  ScrollReveal().reveal('.social', { delay: 500});
+  ScrollReveal().reveal('.footer', { delay: 500});
